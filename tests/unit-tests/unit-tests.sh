d280c2773914390fb98edacfb2f73f11373e27c2
Muhammad Shakeel <137326535+enterprisium@users.noreply.github.com>
2024-01-20 16:41:52 +0500

    Delete tests directory

--- tests/unit-tests/unit-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-tests_succeded=true
-# shellcheck disable=SC2010
-for tst in $(ls "$(dirname "$0")"/test* | grep -v "$(basename "$0")")
-do
-    $tst || tests_succeded=false
-done
-
-$tests_succeded
